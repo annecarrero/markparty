bdabb3d2f0260d5c75b46813a6a91c60efe72315
annely <carreroannely@gmail.com>
2025-03-25 17:55:00 -0300

    configuração do projeto

---
+++ src/app/page.tsx
@@ -0,0 +1,11 @@
+import { Button } from "@/components/ui/button";
+import { Input } from "@/components/ui/input";
+
+export default function(){
+  return(
+    <> 
+    <Button className="h-32">test</Button>
+    <Input/>
+    </>
+  );
+}
